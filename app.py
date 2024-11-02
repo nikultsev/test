bfd0279076bb4782f145f65edde954296b8ae1b0
nikultsev <nikita.nikultsev@gmail.com>
2024-11-02 20:13:54 +1100

    my app

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title('first appe')
+
+username = st.text_input('give me your name')
+
+number = st.slider('select number', 0, 10)
+
+if number % 2 == 0:
+    st.write(f'hello {username}, your number is divisible by 2 ')
+
+
+if number % 2 != 0:
+    st.write(f'hello {username}, your number is not divisible by 2 ')
+
+if st.button('dumb ape button'):
+    st.write('why did you click this button')
